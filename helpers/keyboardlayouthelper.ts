db4393f20d82b78181cc4978a4cc375f656157fa
jarek <39216951+jarek7410@users.noreply.github.com>
2023-06-01 16:47:34 +0200

    progress

---
+++ helpers/keyboardlayouthelper.ts
@@ -0,0 +1,42 @@
+export const buttonTable= [
+    { row: 1,children:[
+            { id: 21, name: "pik", icon:"cards-spade" ,color:"black"},
+            { id: 22, name: "kier", icon:"cards-heart",color:"red" },
+            { id: 23, name: "karo", icon:"cards-diamond",color:"orange" },
+            { id: 24, name: "trefl", icon:"cards-club", color:"green"},
+            { id: 25, name: "bezatu",text:"NT",color:"blue"  },
+        ]
+    },
+    { row: 2,children:[
+            { id: 1, name: "1", icon:"numeric-1" },
+            { id: 2, name: "2",  icon:"numeric-2"  },
+            { id: 3, name: "3",  icon:"numeric-3"  },
+            { id: 19, name: "J, +", text:"J"  },
+            { id: 20, name: "Q, -",  text:"Q"  },
+        ]
+    },
+    { row: 3,children:[
+            { id: 4, name: "4", icon:"numeric-4" },
+            { id: 5, name: "5",  icon:"numeric-5"  },
+            { id: 6, name: "6",  icon:"numeric-6"  },
+            { id: 17, name: "K, pass", text: "K"   },
+            { id: 18, name: "A, =",  text:"A"  },
+        ]
+    },
+    { row: 4,children:[
+            { id: 7, name: "7", icon:"numeric-7" },
+            { id: 8, name: "8",  icon:"numeric-8"  },
+            { id: 9, name: "9",  icon:"numeric-9"  },
+            { id: 15, name: "XX", text:"XX"},
+            { id: 16, name: "X",  text:"X"  },
+        ]
+    },
+    { row: 5,children:[
+            { id: 11, name: "NS", text: "N/S" },
+            { id: 10, name: "10/0",  icon:"numeric-0"  },
+            { id: 12, name: "EW",  text: "E/W" },
+            { id: 14, name: "cansel", text:"CAN\nCEL"   },
+            { id: 13, name: "ok",  text: "OK" },
+        ]
+    },
+];
